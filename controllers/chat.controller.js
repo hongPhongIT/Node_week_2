000a54c13f8ae097101efb8e132421effea41653
hongphongIT <hongphongthptso3@gmail.com>
2018-12-24 01:15:59 +0700

    fix convention code

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -4,13 +4,8 @@ const ChatController = {};
 
 ChatController.getAll = async (req, res) => {
     try {
-        await Chat.find().exec((error, chats) => {
-            if (error) {
-                return res.status(400).json({isSuccess: false, error: e.message});
-            } else {
-                return res.status(200).json({isSuccess: true, chats: chats});
-            }
-        });
+        const chats = await Chat.find();
+        return res.status(200).json({isSuccess: true, chats: chats});
     } catch (e) {
         return res.status(400).json({isSuccess: false, error: e.message});
     }
@@ -19,13 +14,8 @@ ChatController.getAll = async (req, res) => {
 ChatController.getChat = async (req, res) => {
     try {
         const chatId = req.params.id;
-        await Chat.findById(chatId).exec((error, _chat) => {
-            if (error) {
-                return res.status(400).json({error: error.message});
-            } else {
-                return res.status(200).json({isSuccess: true, chat: _chat});
-            }
-        });
+        const chat = await Chat.findById(chatId);
+        return res.status(200).json({isSuccess: true, chat: chat});
     } catch (e) {
         return res.status(400).json({isSuccess: false, error: e.message});
     }
@@ -50,13 +40,8 @@ ChatController.updateChat = async  (req, res) => {
     try {
         const chatId = req.params.id;
         const {name, message } = req.body;
-        await Chat.findByIdAndUpdate(chatId, { $set: { name, message }}, (error, chat) => {
-            if (error) {
-                return res.status(400).json({isSuccess: false, error: error.message});
-            } else {
-                return res.status(200).json({isSuccess: true, chat: chat});
-            }
-        });
+        const chat = await Chat.findByIdAndUpdate(chatId, { $set: { name, message }});
+        return res.status(200).json({isSuccess: true, chat: chat});
     } catch (e) {
         return res.status(400).json({isSuccess: false, error: e.message});
     }
@@ -65,13 +50,8 @@ ChatController.updateChat = async  (req, res) => {
 ChatController.removeChat = async (req, res) => {
     try {
         const chatId = req.params.id;
-        await Chat.findByIdAndRemove(chatId, (error, chat) => {
-            if (error) {
-                return res.status(400).json({isSuccess: false, error: e});
-            } else {
-                return res.status(200).json({isSuccess: true, chat: chat});
-            }
-        });
+        await Chat.findByIdAndRemove(chatId);
+        return res.status(200).json({isSuccess: true, message: 'Delete is success'});
     } catch (e) {
         return res.status(400).json({isSuccess: false, error: e});
     }
